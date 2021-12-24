d8c2be93a5f4651bd42c4b78e800299bf38e1f2a
ChenDataaa <68600402+ChenDataaa@users.noreply.github.com>
2021-12-24 17:46:36 +0800

    JavaWork

--- JavaBasic/src/com.fundation/E_throws.java
+++ JavaBasic/src/com.fundation/E_throws.java
@@ -91,10 +91,23 @@ import java.io.IOException;
  *      // 处理异常方式2
  * }
  * ...
- * // finally可选
  * (finally {
  *      // 一定会执行的代码
  * })
+ *
+ * ④ throws + 异常类型:
+ *    1.写在方法的声明处: public void method() throws Exception...{};
+ *      可能会抛出的异常类型，一旦方法执行出现异常，仍会在异常代码处生成异常类对象，
+ *      此对象满足throws后的异常类型时，就会被抛出，异常后续代码不再执行
+ *
+ *    2. 体会: try-catch-finally:真正的将异常给处理掉了。
+ *       throws的方式只是将异常抛给了方法的调用者。并没有真正将异常处理掉。
+ *
+ *    3. 开发中如何选择使用try-catch-finally还是使用throws？
+ *    3.1 如果父类中被重写的方法没有throws方式处理异常，则子类重写的方法也不能使用throws，
+ *        意味着如果子类重写的方法中有异常，必须使用try-catch-finally方式处理。
+ *    3.2 执行的方法a中，先后又调用了另外的几个方法，这几个方法是递进关系执行的。建议这几个方法使用throws
+ *        的方式进行处理。而执行的方法a可以考虑使用try-catch-finally方式进行处理。
  */
 public class E_throws {
     public static void main(String[] args) {
